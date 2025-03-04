a0962f6e71f06bb593586c16eb728f11dcdb8db2
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2025-03-04 16:50:12 -0300

    Add django-cleanup and django-htmx to project dependencies

--- a_core/settings.py
+++ a_core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django_cleanup.apps.CleanupConfig',
+    'django_htmx',
     'a_pagina'
 ]
 
