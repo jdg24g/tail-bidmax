9d6d4001801171d4f5622c0974e6a104d836e445
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2025-03-04 18:52:34 -0300

    Add WebSocket support with Django Channels and HTMX

---
+++ a_pagina/consumers.py
@@ -0,0 +1,9 @@
+from channels.generic.websocket import WebsocketConsumer
+
+class TicketConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+    
