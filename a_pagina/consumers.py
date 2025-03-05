95e279d09cfb90e4d529891fdfd3e07f7cf30d83
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2025-03-04 22:40:53 -0300

    Add user logging to WebSocket connection and disconnection events

--- a_pagina/consumers.py
+++ a_pagina/consumers.py
@@ -2,8 +2,12 @@ from channels.generic.websocket import WebsocketConsumer
 
 class TicketConsumer(WebsocketConsumer):
     def connect(self):
+        user = self.scope['user']
+        print(f"El usuario: {user} esta conectado")
         self.accept()
 
     def disconnect(self, close_code):
+        user = self.scope['user']
+        print(f"El usuario {user} se desconecto codigo de error {close_code}")
         pass
     
