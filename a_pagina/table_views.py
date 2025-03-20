4a294a14631aa09b4ac65656e129f2ecd223a86c
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2025-03-20 20:06:43 -0300

    Agrega la vista 'ver_tabla_por_mesa' para mostrar tickets pendientes por mesa y actualiza las rutas en 'urls.py'. Se modifica el archivo JavaScript para incluir la nueva ruta en la lista de paths. Se añaden nuevas funciones en 'form_watch_ticket.html' para permitir el cobro de tickets y se mejora el CSS con una nueva clase para el diseño. Además, se realizan ajustes menores en la plantilla para optimizar la presentación de datos.

--- a_pagina/table_views.py
+++ a_pagina/table_views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Ticket, ItemPedido, Producto
 from django.core.paginator import Paginator
+from django.http import HttpResponse
 
 @login_required(login_url="/login/")
 def tabla_delivery(request):
@@ -52,4 +53,39 @@ def tabla_delivery_todos(request):
         'tab_context' : "DELIVERY_TODOS"
     }
     return render(request,"index.html",context)
+    
+
+@login_required(login_url='/login/')
+def ver_tabla_por_mesa(request, mesa=None):
+    if mesa != None:
+        mesa = int(mesa)
+        lista_tickets = Ticket.objects.filter(mesa=mesa,tipo="MESA",caja="PENDIENTE").order_by('-add_time')
+        if lista_tickets.count() == 0:
+            return HttpResponse(f"No hay tickets con esa mesa numero {mesa}")
+        pagintor = Paginator(lista_tickets,5)
+        page_number = request.GET.get('page',1)
+        tickets = pagintor.get_page(page_number)
+        context ={
+            'title' : "BIDMAX - MESA: " + str(mesa),
+            'color': "#0F172A",
+            'items' : tickets,
+            'tab_context' : "PENDIENTE",
+            "mostrar_tabla": True
+        }
+        return render(request,"index.html",context)
+    else:
+        filtro = ["PENDIENTE","A PAGAR"]
+        mesas = Ticket.objects.filter(caja__in=filtro,tipo="MESA").order_by('mesa')
+        context = {
+            'title': "BIDMAX - MESAS PENDIENTES",
+            'color': "#0F172A",
+            'items': mesas,
+            'tab_context': "PENDIENTE",
+            "mostrar_tabla": False
+        }
+        return render(request, "sub_index.html", context)
+
+
+    
+    
     
--- a_pagina/urls.py
+++ a_pagina/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from .views import DeleteTicketView, EditarTicketView,tabla_todos
-from .table_views import tabla_delivery,tabla_cobrado,tabla_delivery_todos
+from .table_views import tabla_delivery,tabla_cobrado,tabla_delivery_todos,ver_tabla_por_mesa
 urlpatterns = [
     path('', views.index, name='index'),
     path('ticket/<int:pk>/', views.detalle_ticket, name='detalle_ticket'),
@@ -18,5 +18,7 @@ urlpatterns = [
     path('tabla-todos/', tabla_todos, name='tabla_todos'),
     path('tabla-delivery-todos/', tabla_delivery_todos, name='tabla_delivery_todos'),
     path('cambiarCajaCobrado/<int:pk>/', views.cambiar_caja_cobrado, name='cambiar_caja_cobrado'),
+    path('ver-tabla-por-mesa/', ver_tabla_por_mesa, name='ver_tabla_por_mesa'),
+    path('ver-tabla-por-mesa/<int:mesa>/', ver_tabla_por_mesa, name='tabla_por_mesas'),
 
 ]
